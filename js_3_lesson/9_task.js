6c55fe233f829df8abdeaca9c75f1f0976dd8751
Milk Midav <wadim017@gmail.com>
2023-01-14 18:47:15 +0200

    fix type conversions

--- js_3_lesson/9_task.js
+++ js_3_lesson/9_task.js
@@ -4,14 +4,10 @@
 
 const str = "025468";
 
-const toNum = parseInt(str);
-
-let newArr = [];
+const newArr = [];
 
 for (let i = 0; i < str.length; i++) {
-  console.log(newArr);
-  if (str[i] % 2 === 0 && str[i - 1] % 2 === 0) {
-    
+  if (Number(str[i]) % 2 === 0 && Number(str[i - 1]) % 2 === 0) {
     newArr.push('-', str[i]);
   } else {
     newArr.push(str[i]);
