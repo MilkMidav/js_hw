d1a213cb2bd3c1ee982e9124a86f0ad2fd5e830b
Milk Midav <wadim017@gmail.com>
2023-01-17 18:11:56 +0200

    changed name of function and сhanged let to const

--- js_4_lesson/3_task.js
+++ js_4_lesson/3_task.js
@@ -2,10 +2,10 @@
  * Return a new array containing emails with the given domain name */
 'use strict'
 
-function getEmailDomain(array, domain) {
+function filterByDomain(array, domain) {
   const endWith = '@' + domain;
 
-  let result = [];
+  const result = [];
 
   for (let i = 0; i < array.length; i++) {
     if (array[i].endsWith(endWith)) {
@@ -13,11 +13,11 @@ function getEmailDomain(array, domain) {
     }
   }
   
-  return result.length === 0 ? console.log("domain is not found") : result
+  return result;
 }
 
 const arr = ['steve@yahoo.com', 'jack@google.com', 'kate@mailbox.com', 'luke@google.com'];
 
-const result = getEmailDomain(arr, 'yahoo.com')
+const result = filterByDomain(arr, 'yahoo.com')
 
 console.log(result)
