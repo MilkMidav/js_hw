7119137cf78d13bd1b685506a1efd3e9427120fc
Milk Midav <wadim017@gmail.com>
2023-01-26 17:37:16 +0200

    added strict mode

--- codewars/XO.js
+++ codewars/XO.js
@@ -1,23 +1,24 @@
 /**Check to see if a string has the same amount of 'x's and 'o's.
  *  The method must return a boolean and be case insensitive. 
  * The string can contain any char. */
+"use strict"
 
 function XO(str) {
   const toLower = str.toLowerCase()
   
-  let counterX = 0
-  let counterO = 0
+  let counterX = 0;
+  let counterO = 0;
 
   for (let i = 0; i < toLower.length; i++) {
     if (toLower[i] === 'o') {
-      counterO++
+      counterO++;
     }
     if (toLower[i] === 'x') {
-      counterX++
+      counterX++;
     }
   }
 
-  return counterX === counterO
+  return counterX === counterO;
 }
 
-console.log(XO("xxo"))
+console.log(XO("xxo"));
