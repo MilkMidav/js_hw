f69454afd7a7ccb5c36d152e8ad869ceab5ca82b
Milk Midav <wadim017@gmail.com>
2023-01-26 17:40:27 +0200

    fix style problems

--- codewars/sumOfTwoSmallestNumbers.js
+++ codewars/sumOfTwoSmallestNumbers.js
@@ -1,7 +1,7 @@
+"use strict"
+
 function sumTwoSmallestNumbers(array) {  
-  
   let smallest = array[0];
-  
   let secondSmallest = array[1];
 
   for (let i = 0; i < array.length; i++) {
@@ -15,28 +15,10 @@ function sumTwoSmallestNumbers(array) {
       secondSmallest = array[i];
     }
   }
+
   return secondSmallest + smallest;
 }
 
 const arr = [23, 71, 33, 82, 1];
 
 console.log(sumTwoSmallestNumbers(arr))
-
-/** SECOND SOLUTION*/
-
-function sumTwoSmallestNumbers2(array){
-  let temp;
-
-  for(let i = 0; i < array.length; i++){
-    for(let j = i + 1; j < array.length; j++){
-      if(array[i] > array[j]){
-        temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-      }
-    }
-  }  
-  return array[0] + array [1]
-}
-
-console.log(sumTwoSmallestNumbers2(arr))
