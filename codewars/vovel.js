664c989cdea3febf2a75bdf2f5a011caac295715
Milk Midav <wadim017@gmail.com>
2023-01-26 17:35:41 +0200

    remove indexOf and added includes

--- codewars/vovel.js
+++ codewars/vovel.js
@@ -1,17 +1,18 @@
 /**Check the number of vowels in the string. 
- * The method must return a boolean value and be case insensitive. 
+ * The method must be case insensitive. 
  * The string can contain any character. */
+"use strict"
 
 function vowelCount(string) {
   const vowels = 'aeiouAEIOU';
   let counter = 0;
   
   for(let i = 0; i < string.length ; i++) {
-    if (vowels.indexOf(string[i]) !== -1) {
+    if (vowels.includes(string[i])) {
       counter += 1;
     }
-  
   }
+  
   return counter;
 }
 
