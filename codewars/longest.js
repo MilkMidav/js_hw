2d8f2deb44d10fbfdc55ab70b208cec9f66cada8
Milk Midav <wadim017@gmail.com>
2023-01-26 17:38:20 +0200

    fix style problems

--- codewars/longest.js
+++ codewars/longest.js
@@ -1,24 +1,26 @@
 /** Take 2 strings s1 and s2 including only letters from a to z. 
  * Return a new sorted string, the longest possible, containing distinct
  *  letters - each taken only once - coming from s1 or s2. */
+"use strict"
 
 function longest(s1, s2) {
-  const arr1 = s1.toLowerCase().split("")
-  const arr2 = s2.toLowerCase().split("")
+  const arr1 = s1.toLowerCase().split("");
+  const arr2 = s2.toLowerCase().split("");
 
-  const childrenArr = arr1.concat(arr2)
+  const childrenArr = arr1.concat(arr2);
 
-  const result = []
+  const result = [];
 
   for (let i = 0; i < childrenArr.length; i++) {
     if (!result.includes(childrenArr[i])) {
-      result.push(childrenArr[i])
+      result.push(childrenArr[i]);
     }
   }
-  return result.sort().join('')
+  
+  return result.sort().join('');
 }
 
-const a = "xyaabBbcKcccdefww"
-const b = "xxxxyyyyablmopq"
+const a = "xyaabBbcKcccdefww";
+const b = "xxxxyyyyablmopq";
 
-console.log(longest(a, b))
+console.log(longest(a, b));
