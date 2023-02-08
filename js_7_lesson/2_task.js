ff6029ad424bfed5002dc7ac348611573a51eaa6
Milk Midav <wadim017@gmail.com>
2023-02-08 16:36:16 +0200

    added 2 task

---
+++ js_7_lesson/2_task.js
@@ -0,0 +1,12 @@
+/**Given an array of numbers, get rid of all odd numbers: */
+'use strict'
+
+function removeOdds(array) {
+  const odd = array.filter(function(element) {
+    return element % 2 === 0;
+  })
+
+  return odd;
+}
+
+console.log(removeOdds([1, 2, 3, 4, 5, 6, 7, 8]));
