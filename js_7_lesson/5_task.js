a2fba9bd2d7315490532a4ced01910fd56410f17
Milk Midav <wadim017@gmail.com>
2023-02-08 18:29:14 +0200

    added 5 task

---
+++ js_7_lesson/5_task.js
@@ -0,0 +1,38 @@
+/**Given an array of people, return a string containing all people's names separated by commas: */
+'use strict'
+
+function getNames(array) {
+  const names = array.map(function(person) {
+    return person.name;
+  });
+
+  return names.join(', ');
+}
+
+console.log(getNames([
+  {
+    id: 1,
+    name: 'Kate',
+    age: 22,
+  },
+  {
+    id: 2,
+    name: 'Bruce',
+    age: 9,
+  },
+  {
+    id: 3,
+    name: 'Ian',
+    age: 16,
+  },
+  {
+    id: 4,
+    name: 'John',
+    age: 48,
+  },
+  {
+    id: 5,
+    name: 'Stephen',
+    age: 31
+  }
+]));
