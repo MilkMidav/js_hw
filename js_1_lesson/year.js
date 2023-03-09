b1c3b1a4e93287979d117a26625dd6a1b40d5523
Milk Midav <wadim017@gmail.com>
2023-03-09 14:40:34 +0200

    added space

--- js_1_lesson/year.js
+++ js_1_lesson/year.js
@@ -1,9 +1,9 @@
 let input = prompt("Enter year:");
 
-if((input % 4 === 0) || (input % 400 === 0)) {
+if ((input % 4 === 0) || (input % 400 === 0)) {
   alert("It's a leap year")
   console.log("It's a leap year");
-}else {
+} else {
   alert("it's not a leap year")
   console.log ("it's not a leap year")
 }
