3847720a17589ee9f11346c5340a0e606d6588d0
Milk Midav <wadim017@gmail.com>
2023-03-31 17:22:08 +0300

    wrote html markup, added styles with tailwind, replaced buggy images with new ones,added a font

---
+++ dom_1_lesson/src/index.js
@@ -0,0 +1,12 @@
+const items = [
+  { id: 1, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 2, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 3, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 4, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 5, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 6, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 7, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 8, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 9, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 10, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+];
