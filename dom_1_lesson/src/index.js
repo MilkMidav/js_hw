c94bf269f6a17db8a5d76db086bbe694c411b265
Milk Midav <wadim017@gmail.com>
2023-03-31 17:52:54 +0300

    new folder for card img, added the path to the pictures in the items variable

--- dom_1_lesson/src/index.js
+++ dom_1_lesson/src/index.js
@@ -1,12 +1,16 @@
 const items = [
-  { id: 1, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 2, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 3, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 4, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 5, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 6, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 7, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 8, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 9, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
-  { id: 10, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'blank' },
+  { id: 1, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_1.png' },
+  { id: 2, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_2.png' },
+  { id: 3, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_3.png' },
+  { id: 4, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_4.png' },
+  { id: 5, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_5.png' },
+  { id: 6, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_1.png' },
+  { id: 7, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_2.png' },
+  { id: 8, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_3.png' },
+  { id: 9, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_4.png' },
+  { id: 10, brand: 'Lamere I', title: 'Recycle Boucle Knit Cardigan Pink', rating: 4.5, price: 120, imgSrc: 'img/card_img/product_img_5.png' },
 ];
+
+function createList(item) {
+  let card = document.createElement('div')
+}
