7de1dcafac9fc8b003fab7f555bca03053e7355f
Milk Midav <wadim017@gmail.com>
2023-04-25 14:48:31 +0300

    a little refractoring

--- dom_lesson_2/index.js
+++ dom_lesson_2/index.js
@@ -8,32 +8,31 @@ const imgData = [
   { id: 7, src: 'img/slide_7.jpg' },
 ];
 
-function createSlider(array, id) {
-  const slider = document.querySelector(`#${id}`);
+function createSlider(id, array) {
+  const slider = document.getElementById(`${id}`);
 
   const galleryContainer = document.createElement('div');
   galleryContainer.classList.add('slider__gallery');
 
-  const navContainer = document.createElement('div');
-  navContainer.classList.add('slider__nav_container');
+  const nav = document.createElement('div');
+  nav.classList.add('slider__nav_container');
 
   const nextButtonElem = document.createElement('button');
   const prevButtonElem = document.createElement('button');
-  nextButtonElem.innerHTML = '&#8658;';
-  prevButtonElem.innerHTML = '&#8656;';
+  nextButtonElem.textContent = '⇒';
+  prevButtonElem.textContent = '⇐';
   nextButtonElem.classList.add('slider__button', 'slider__button--right');
   prevButtonElem.classList.add('slider__button', 'slider__button--left');
 
   const mainImg = document.createElement('img');
-  mainImg.classList.add('main_img');
-  mainImg.src = '';
-  mainImg.alt = 'main_img';
+  mainImg.classList.add('slider__hero');
+  mainImg.alt = 'slider__hero';
   
-  navContainer.appendChild(prevButtonElem);
-  navContainer.appendChild(mainImg);
-  navContainer.appendChild(nextButtonElem);
+  nav.appendChild(prevButtonElem);
+  nav.appendChild(mainImg);
+  nav.appendChild(nextButtonElem);
 
-  slider.appendChild(navContainer);
+  slider.appendChild(nav);
   slider.appendChild(galleryContainer);
 
   const slidesGallery = document.querySelector('.slider__gallery');
@@ -46,13 +45,12 @@ function createSlider(array, id) {
     visibleSlides: 4,
   };
 
-  if (window.matchMedia("(max-width: 768px)").matches)  currentSlide.visibleSlides = 3;
+  if (window.matchMedia("(max-width: 768px)").matches) currentSlide.visibleSlides = 3;
    
   if (window.matchMedia("(max-width: 468px)").matches) currentSlide.visibleSlides = 2;
   
-  const createSlideNav = (array) =>  {
-    const stepForward = currentSlide.stepForward;
-    const stepBack = currentSlide.stepBack;
+  const createSlidesArray = (array) =>  {
+    const { stepForward, stepBack } = currentSlide;
     const toShowFromStart = currentSlide.visibleSlides - (array.length - stepForward);
     const toShowFromEnd = (array.length - stepBack) - currentSlide.visibleSlides;
 
@@ -68,22 +66,28 @@ function createSlider(array, id) {
   };
 
   const updateSliderImages = (array) => {
-    const visibleSlidesArr = createSlideNav(array);
+    const updatedSlides = createSlidesArray(array);
      
     const slidesGallery = document.querySelector('.slider__gallery');
-    const mainSlide = document.querySelector('.main_img');
+    const mainSlide = document.querySelector('.slider__hero');
     const images = slidesGallery.querySelectorAll('img');
 
-    mainSlide.src = visibleSlidesArr[0].src;
-    for (let i = 0; i < visibleSlidesArr.length; i++) {
+    mainSlide.src = updatedSlides[0].src;
+
+    const img = document.createElement('img');
+
+    img.src = updatedSlides[0].src;
+    img.classList.add('gallery__img', 'gallery__img--active');
+    slidesGallery.appendChild(img);
+
+    for (const slide of updatedSlides.slice(1)) {
       const img = document.createElement('img');
-    
-      img.src = visibleSlidesArr[i].src;
+
+      img.src = slide.src;
       img.classList.add('gallery__img');
       slidesGallery.appendChild(img);
-
-      if (i === 0) img.classList.add('gallery__img--active');
     }
+
     images.forEach(img => {
       img.remove();
     });
@@ -157,4 +161,4 @@ function createSlider(array, id) {
   return;
 }
 
-createSlider(imgData, 'slider');
+createSlider('slider', imgData);
