0e53a4fb6a20dd55356662323a086700c6a0a8a7
Milk Midav <wadim017@gmail.com>
2023-05-23 23:15:21 +0300

    added hw

---
+++ asynchronous _js_lesson_1/index.js
@@ -0,0 +1,48 @@
+const robot = {
+  isActive: false,
+  actions: [], 
+  walk: function(timeout = 1) {
+    if (this.isActive) {
+      this.actions.push({ action: 'walking', timeout: timeout });
+    } else {
+      this.isActive = true;
+      console.log(`Started walking for ${timeout} seconds`);
+      
+      setTimeout(() => {
+        console.log('Ended walking');
+        this.setActionsTimeout();
+      }, timeout * 1000);
+    }
+
+    return this;
+  },
+  idle: function(timeout = 1) {
+    if (this.isActive) {
+      this.actions.push({ action: 'idling', timeout: timeout });
+    } else {
+      this.isActive = true;
+      console.log(`Started idling for ${timeout} seconds`);
+
+      setTimeout(() => {
+        console.log('Ended idling');
+        this.setActionsTimeout();
+      }, timeout * 1000);
+    }
+
+    return this;
+  },
+  setActionsTimeout: function() {
+    if (this.actions.length > 0) {
+      const action = this.actions.shift();
+      console.log(`Started ${action.action} for ${action.timeout} seconds`);
+      setTimeout(() => {
+        console.log(`Ended ${action.action}`);
+        return this.setActionsTimeout();
+      }, action.timeout * 1000);
+    }
+
+    return;
+  }
+};
+
+robot.walk(3).idle(3).walk(3);
