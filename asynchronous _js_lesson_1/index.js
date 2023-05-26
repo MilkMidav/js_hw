d71868d315e0e7dad50d5db560ea6d5ffe23eef4
Milk Midav <wadim017@gmail.com>
2023-05-26 21:46:24 +0300

    code refactoring was done

--- asynchronous _js_lesson_1/index.js
+++ asynchronous _js_lesson_1/index.js
@@ -1,50 +1,33 @@
 const robot = {
   isActive: false,
-  actions: [], 
-  walk: function(timeout = 1) {
-    if (this.isActive) {
-      this.actions.push({ action: 'walking', timeout: timeout });
-    } else {
-      this.isActive = true;
-      console.log(`Started walking for ${timeout} seconds`);
-
-      setTimeout(() => {
-        console.log('Ended walking');
-        this.setActionsTimeout();
-      }, timeout * 1000);
-    }
+  actions: [],
+  walk: function (timeout = 1) {
+    this.actions.push({ action: 'walking', timeout: timeout });
+    if (!this.isActive) this.startNextTask();
+    
+    return this;
+  }, 
+  idle: function (timeout = 1) {
+    this.actions.push({ action: 'idling', timeout: timeout });
+    if (!this.isActive) this.startNextTask();
 
     return this;
   },
-  idle: function(timeout = 1) {
-    if (this.isActive) {
-      this.actions.push({ action: 'idling', timeout: timeout });
-    } else {
-      this.isActive = true;
-      console.log(`Started idling for ${timeout} seconds`);
-
-      setTimeout(() => {
-        console.log('Ended idling');
-        this.setActionsTimeout();
-      }, timeout * 1000);
-    }
-
-    return this;
-  },
-  setActionsTimeout: function() {
+  startNextTask: function() {
     if (this.actions.length > 0) {
       const action = this.actions.shift();
+      this.isActive = true;
       console.log(`Started ${action.action} for ${action.timeout} seconds`);
+
       setTimeout(() => {
         console.log(`Ended ${action.action}`);
-        return this.setActionsTimeout();
+        this.startNextTask();
       }, action.timeout * 1000);
     } else {
-      console.log('No tasks to execute')
+      console.log('No tasks to execute');
+      this.isActive = false;
     }
-
-    return;
-  }
+  },
 };
 
 robot.walk(3).idle(3).walk(3);
