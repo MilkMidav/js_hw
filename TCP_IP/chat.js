a288c1a9123efb39bb8f117fdc8377bbe8bd025e
Milk Midav <wadim017@gmail.com>
2023-06-27 18:52:48 +0300

    Refactor startChat function to improve code structure and readability

--- TCP_IP/chat.js
+++ TCP_IP/chat.js
@@ -1,14 +1,14 @@
 const net = require('net');
 
-function startChat() {
-  const hostname = 'localhost';
-  const [username, port, receiverPort] = process.argv.slice(2);
+const hostname = 'localhost';
+const [username, port, receiverPort] = process.argv.slice(2);
 
+function startChat(username, port, receiverPort, hostname) {
   const server = net.createServer((socket) => {
     socket.on('data', data => {
       const { message, sender, handshake } = JSON.parse(data.toString());
 
-      if (handshake && !message) {
+      if (handshake) {
         console.log(`${sender} has entered the chat room`);
       } else {
         console.log(`${sender}: ${message}`);
@@ -20,43 +20,37 @@ function startChat() {
     console.log(`Accepting connecting on ${hostname}:${port}`);
   });
   
-  function connectToServer() {
-    const socket = new net.Socket();
+  const socket = new net.Socket();
     
-    socket.connect(receiverPort, hostname, () => {
-      console.log(`Connected to a friend at ${hostname}:${receiverPort}`);
-      
-      const messageConnections = {
-        sender: username,
-        handshake: true
+  socket.connect(receiverPort, hostname, () => {
+    console.log(`Connected to a friend at ${hostname}:${receiverPort}`);
+    
+    const messageConnections = {
+      sender: username,
+      handshake: true
+    };
+
+    socket.write(JSON.stringify(messageConnections));
+
+    process.stdin.on('data', data => {
+      const message = {
+        message: data.toString(),
+        sender: username
       };
-
-      socket.write(JSON.stringify(messageConnections));
-
-      process.stdin.on('data', data => {
-        const message = {
-          message: data.toString(),
-          sender: username
-        };
-        
-        socket.write(JSON.stringify(message));
-      });
+      
+      socket.write(JSON.stringify(message));
     });
-    
-    socket.on('end', () => {
-      console.log('Friend left the chat room');
-      setTimeout(connectToServer, 5000);
-    });
-    
-    socket.on('error', () => {
-      console.log('Connection to a friend was refused. Retrying in 5 second');
-      setTimeout(connectToServer, 5000);
-    });
-    
-    return;
-  }
-
-  connectToServer();
+  });
+  
+  socket.on('end', () => {
+    console.log('Friend left the chat room');
+    setTimeout(() => socket.connect(receiverPort, hostname), 5000);
+  });
+  
+  socket.on('error', () => {
+    console.log('Connection to a friend was refused. Retrying in 5 second');
+    setTimeout(() => socket.connect(receiverPort, hostname), 5000);
+  });
 }
 
-startChat();
+startChat(username, port, receiverPort, hostname);
